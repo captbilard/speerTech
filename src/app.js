c993b0a310c197144eb869116bdb065bab53e43a
bilard <bilardosinulu@gmail.com>
2023-01-05 22:35:00 -0700

    initial setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+module.exports = app;
